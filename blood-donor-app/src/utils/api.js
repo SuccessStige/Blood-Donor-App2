6399ba983128ac7befd97a3c29c35937f9e70f82
SuckyStige <stigehub@gmail.com>
2026-01-12 22:42:31 +0100

    Updating Code

--- blood-donor-app/src/utils/api.js
+++ blood-donor-app/src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "http://54.173.188.175/api/",
   headers: {
     "Content-Type": "application/json",
   },
