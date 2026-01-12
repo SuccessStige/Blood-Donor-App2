bca229c9ecdc7de5d27ba2c8160d44caddd2dc27
SuckyStige <stigehub@gmail.com>
2026-01-12 20:32:41 +0100

    Urls Code Update

--- bloodBackend/bloodBackend/urls.py
+++ bloodBackend/bloodBackend/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.urls import path, include
 from django.contrib import admin
+from django.conf import settings # new
+from  django.conf.urls.static import static #new
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -31,3 +33,6 @@ urlpatterns = [
     path("api/", include("blood.urls")),
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
